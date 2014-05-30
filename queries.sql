ae620b377134a8bacf6713eb06d3261d6d6eab9a
zephiro309 <zephiro309@gmail.com>
2014-05-30 16:30:21 +0100

    Added query for team leads.

--- queries.sql
+++ queries.sql
@@ -2,5 +2,10 @@ SELECT * FROM Funcionario; --Seleciona as informacoes de todos os funcionarios
 
 
 SELECT COUNT(*)  FROM Jogo --Conta os jogos com idadeMin <= 18
-	WHERE(idadeMin <= 18);
-	
+WHERE(idadeMin <= 18);
+
+SELECT * FROM Funcionario
+WHERE funcionarioID = (
+	SELECT funcionario FROM FuncionarioEquipa
+	WHERE(projectLead = 1)
+	);
