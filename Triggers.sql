5801cbf880503d9cdae4be16ad278f0406ccbd8c
Gabriel <gabrielsouto100d@gmail.com>
2014-06-01 22:25:55 +0100

    triggers melhor comentados

--- Triggers.sql
+++ Triggers.sql
@@ -5,36 +5,36 @@ DROP TRIGGER if exists apagaDerivadosDoJogo;
 DROP TRIGGER if exists apagaDerivadosDoModulo;
 
 
-CREATE TRIGGER apagaDerivadosDoDepartamento
+CREATE TRIGGER apagaDerivadosDoDepartamento --apaga classes que apontam para Departamento
  BEFORE DELETE ON Departamento
  FOR EACH ROW BEGIN
-      DELETE FROM Funcionario WHERE (Funcionario.departamento = OLD.departamentoID);
-      DELETE FROM Modulo WHERE (Modulo.departamento = OLD.departamentoID);
+      DELETE FROM Funcionario WHERE (Funcionario.departamento = OLD.departamentoID); --apaga Funcionario
+      DELETE FROM Modulo WHERE (Modulo.departamento = OLD.departamentoID); --apaga modulo
  END;
 
- CREATE TRIGGER apagaDerivadosDaEquipa
+ CREATE TRIGGER apagaDerivadosDaEquipa --apaga classes que apontam para Equipa
  BEFORE DELETE ON Equipa
  FOR EACH ROW BEGIN
-      DELETE FROM FuncionarioEquipa WHERE(FuncionarioEquipa.equipa = OLD.equipaID);
-      UPDATE Jogo SET equipa = NULL WHERE(Jogo.equipa = OLD.equipaID);
+      DELETE FROM FuncionarioEquipa WHERE(FuncionarioEquipa.equipa = OLD.equipaID); --Apaga FuncionarioEquipa
+      UPDATE Jogo SET equipa = NULL WHERE(Jogo.equipa = OLD.equipaID);-- Muda o apontador do jogo para NULL
    --   UPDATE Jogo SET equipa = NULL WHERE (Jogo.equipa = OLD.equipaID);
  END;
 
 
-CREATE TRIGGER apagaDerivadosDoFuncionario
+CREATE TRIGGER apagaDerivadosDoFuncionario --apaga classes que apontam para o funcionario
  BEFORE DELETE ON Funcionario
  FOR EACH ROW BEGIN
-      DELETE FROM FuncionarioEquipa WHERE(FuncionarioEquipa.funcionario = OLD.funcionarioID);
+      DELETE FROM FuncionarioEquipa WHERE(FuncionarioEquipa.funcionario = OLD.funcionarioID); 
       DELETE FROM DepartamentoFuncionario WHERE (DepartamentoFuncionario.departamento = OLD.departamento);
       
  END;
 --CREATE TRIGGER db
 
 
-CREATE TRIGGER apagaDerivadosDoJogo
+CREATE TRIGGER apagaDerivadosDoJogo --apaga classes que apontam para jogo
  BEFORE DELETE ON Jogo
  FOR EACH ROW BEGIN
-      DELETE FROM JogoGenero WHERE(JogoGenero.jogoID = OLD.jogoID);
+      DELETE FROM JogoGenero WHERE(JogoGenero.jogoID = OLD.jogoID); 
       DELETE FROM JogoTipo WHERE(JogoTipo.jogoID = OLD.jogoID);
       DELETE FROM JogoDistribuidor WHERE(JogoDistribuidor.jogoID = OLD.jogoID);
       DELETE FROM JogoModulo WHERE(JogoModulo.jogoID = OLD.jogoID);
@@ -42,7 +42,7 @@ CREATE TRIGGER apagaDerivadosDoJogo
  END;
 
 
- CREATE TRIGGER apagaDerivadosDoModulo
+ CREATE TRIGGER apagaDerivadosDoModulo -- apaga classes que apontam para modulo
  BEFORE DELETE ON Modulo
  FOR EACH ROW BEGIN
        DELETE FROM JogoModulo WHERE(JogoModulo.moduloID = OLD.moduloID);
